feaf660ddf99137da2f2627a191f66f0e1610aea
Deepak Gupta <dgupta25@in.ibm.com>
2021-01-08 17:46:30 +0530

    changed for feature branch

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          OK There!.
+          for feature branch!.
         </p>
         <a
           className="App-link"
